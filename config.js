294e1967fc336c17b5c93fd6b5facedb2a9fb967
Hrafnir <eirik.gaarde@gmail.com>
2026-01-16 12:35:18 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+/* Version: #6 */
+// === FIREBASE CONFIGURATION ===
+// Denne filen inneholder tilkoblingsdetaljer for Firebase.
+// Hvis du bruker GitHub, bør denne filen legges til i .gitignore.
+export const firebaseConfig = {
+apiKey: "AIzaSyBDX-8KtSLoPI2iue0_95Y98pO53AZsOIM",
+authDomain: "makeradmin-e8786.firebaseapp.com",
+projectId: "makeradmin-e8786",
+storageBucket: "makeradmin-e8786.firebasestorage.app",
+messagingSenderId: "116449307799",
+appId: "1:116449307799:web:7b1d1b3c4464b258124dd2",
+measurementId: "G-503WKMZWB5"
+};
+console.log("[Config] Firebase-konfigurasjon lastet.");
+/* Version: #6 */
